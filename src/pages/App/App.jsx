97581b108f959de873e9608f96eb3759c0063c12
kmcaloney15 <katymcaloney@gmail.com>
2022-07-24 20:24:00 -0400

    signup form created

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 import { useState } from 'react'
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+// import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import HomePage from '../HomePage/HomePage';
 import { Routes, Route } from 'react-router-dom'
 import NavBar from '../../components/NavBar/NavBar';
-import { getUser } from '../../utilities/users-service'
+import { getUser } from '../../utilities/users-service';
 
 export default function App() {
   const [user, setUser] = useState(getUser())
@@ -16,7 +16,7 @@ export default function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/home" element={<HomePage />} />
           </Routes>
       </>
       :
