460e1e5092470995f31dd2d625e6e124e3365e46
kmcaloney15 <katymcaloney@gmail.com>
2022-07-27 15:38:47 -0400

    all new recipe form blocks added

--- src/components/NewRecipeForm/NewRecipeForm.jsx
+++ src/components/NewRecipeForm/NewRecipeForm.jsx
@@ -76,102 +76,212 @@ export default function NewRecipeForm({ setUpdated, allRecipes }) {
           className="border-black border-[1px] rounded-md py-4 px-4 font-light text-justify"
           id="hardshadow"
         >
-            {/* NAME */}
-          <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Name
-          </label>
-          <input
-            type="text"
-            name="title"
-            value={formData.name}
-            placeholder="write here..."
-            className="bg-[#f7f7f2] border-b-[1px] border-black outline-0"
-          />
+          {/* NAME */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Name
+            </label>
+            <input
+              type="text"
+              name="title"
+              value={formData.name}
+              placeholder="write here..."
+              className="bg-[#f7f7f2] border-b-[1px] border-black outline-0"
+            />
+          </div>
           {/* CATEGORY */}
-          <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Category:
-          </label>
-          <input
-            type="text"
-            name="categories"
-            value={formData.categories}
-            placeholder="write here..."
-            className="bg-[#f7f7f2]  border-b-[1px] border-black outline-0"
-          />
-{/* SERVINGS */}
-<label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Servings:
-          </label>
-          <input
-            type="text"
-            name="categories"
-            value={formData.servings}
-            placeholder="write here..."
-            className="bg-[#f7f7f2]"
-          />
-
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Category:
+            </label>
+            <input
+              type="text"
+              name="categories"
+              value={formData.categories}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]  border-b-[1px] border-black outline-0"
+            />
+          </div>
+          {/* SERVINGS */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Servings:
+            </label>
+            <input
+              type="text"
+              name="servings"
+              value={formData.servings}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
           {/* RATING */}
-           <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Rating:
-          </label>
-          <input
-            type="number"
-            name="categories"
-            value={formData.categories}
-            placeholder="write here..."
-            className="bg-[#f7f7f2]"
-          />
-           <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Category:
-          </label>
-          <input
-            type="text"
-            name="categories"
-            value={formData.categories}
-            placeholder="write here..."
-            className="bg-[#f7f7f2]"
-          />
-           <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Category:
-          </label>
-          <input
-            type="text"
-            name="categories"
-            value={formData.categories}
-            placeholder="write here..."
-            className="bg-[#f7f7f2]"
-          />
-          <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-          Servings
-          </label>
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Rating:
+            </label>
+            <input
+              type="number"
+              name="rating"
+              value={formData.categories}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Difficulty */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Difficulty:
+            </label>
+            <input
+              type="text"
+              name="difficulty"
+              value={formData.difficulty}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Difficulty */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Difficulty:
+            </label>
 
-          <select
-            name="servings"
-            value={formData.servings}
-            className="font-extralight text-2l text-left h-1/2 px-2 py-2 bg-[#f7f7f2]"
-          >
-            {/* // if I want to include categories as a reference */}
-            {/* {allCats.map((cat) => (
+            <select
+              name="difficulty"
+              value={formData.difficulty}
+              placeholder="Select One"
+              className="font-extralight text-2l text-left h-1/2 px-2 py-2 bg-[#f7f7f2]"
+            >
+              {/* // if I want to include categories as a reference */}
+              {/* {allCats.map((cat) => (
               <option value={cat._id} key={cat._id}>
                 {cat.title}
               </option>
             ))}  */}
 
-            <option value="Easy">Easy</option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
-          </select>
-
-          <label className="font-light text-left text-lg h-1/2 px-2 py-2">
-            Description
-          </label>
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            placeholder="write here..."
-            className="bg-[#f7f7f2] outline-0"
-          />
+              <option value="Easy">Easy</option>
+              <option value="Medium">Medium</option>
+              <option value="Hard">Hard</option>
+            </select>
+          </div>
+          {/* Prep Time */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Prep Time:
+            </label>
+            <input
+              type="text"
+              name="prepTime"
+              value={formData.prepTime}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Cook Time */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Cook Time:
+            </label>
+            <input
+              type="text"
+              name="cookTime"
+              value={formData.cookTime}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Total Time */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Total Time:
+            </label>
+            <input
+              type="text"
+              name="totalTime"
+              value={formData.totalTime}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Source */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Source:
+            </label>
+            <input
+              type="text"
+              name="source"
+              value={formData.source}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Source URL */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Source URL:
+            </label>
+            <input
+              type="text"
+              name="sourceURL"
+              value={formData.sourceUrl}
+              placeholder="write here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Ingredients */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Ingredients:
+            </label>
+            <input
+              type="text"
+              name="ingredient"
+              value={formData.ingredient}
+              placeholder="Add ingredients here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Description */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Description
+            </label>
+            <input
+              type="text"
+              name="description"
+              value={formData.description}
+              placeholder="write the description here..."
+              className="bg-[#f7f7f2] outline-0"
+            />
+          </div>
+          {/* Directions */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Total Time:
+            </label>
+            <input
+              type="text"
+              name="directions"
+              value={formData.directions}
+              placeholder="Add directions here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
+          {/* Notes */}
+          <div>
+            <label className="font-light text-left text-lg h-1/2 px-2 py-2">
+              Notes:
+            </label>
+            <input
+              type="text"
+              name="notes"
+              value={formData.notes}
+              placeholder="Add notes here..."
+              className="bg-[#f7f7f2]"
+            />
+          </div>
           <p>&nbsp;</p>
           <button
             type="submit"
