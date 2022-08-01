93b6f26c5a812853916c2770a059fa60e11d6e07
kmcaloney15 <katymcaloney@gmail.com>
2022-08-01 19:25:52 -0400

    updated auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import Logo from "../../components/Logo/Logo";
+import { Link } from "react-router-dom";
 import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
@@ -10,21 +11,39 @@ export default function AuthPage({ setUser }) {
   return (
     <main className="AuthPage">
       <div className="auth-left ">
-        <Logo />
-        <br></br>
-        <h3>Welcome to Sugar and Spice! </h3>
-      <br></br>
-        {showLogin ? (
-          <SignUpForm setUser={setUser} />
-        ) : (
-          <LoginForm setUser={setUser} />
-        )}
-        <button
-          onClick={() => setShowLogin(!showLogin)}
-          className=" text-white font-medium py-2 px-4 rounded hover:ring hover:ring-orange-400"
-        >
-          {showLogin ? "LOG IN" : "SIGN UP"}
-        </button>
+        <>
+          <Logo />
+          <br></br>
+          <h3>Welcome to Sugar and Spice! </h3>
+          <br></br>
+          {showLogin ? (
+            <>
+              <LoginForm setUser={setUser} />
+              <h3>
+                Don't have an account? &nbsp;&nbsp;
+                <a
+                  href="#"
+                  onClick={() => setShowLogin(!showLogin)}
+                  className="underline text-sage-green-400"
+                >
+                  Sign Up
+                </a>
+              </h3>
+            </>
+          ) : (
+            <>
+              <SignUpForm setUser={setUser} />
+              <h3>
+                Already have an account? &nbsp;&nbsp; <a
+                href="#"
+                onClick={() => setShowLogin(!showLogin)}
+                className="underline text-sage-green-400"
+              >
+                Log In
+              </a></h3>
+            </>
+          )}
+        </>
       </div>
     </main>
   );
